7a6c717da12ee576b45b4672733ee68cbf0448f9
nammar1 <sajinq@outlook.com>
2025-05-24 19:40:35 -0700

    Deploted???

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -26,7 +26,7 @@ app.use((err: Error, req: express.Request, res: express.Response, next: express.
   res.status(500).json({ message: 'Something went wrong!' });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
